cf7537b20e3af0f36738ad5732dd215695cb360d
unknown <akalbarczyk@hotmail.com>
2015-03-31 20:51:17 +0200

    EnemyAI, obracanie sie na przeciwnika

---
+++ LaboratoryNight/Assets/Scripts/TurnAtPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnAtPlayer : MonoBehaviour {
+    private Transform target, myPosition;
+    public int rotationSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        target = go.transform;
+        myPosition = transform; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(target.position, transform.position) < 15F)
+        myPosition.rotation = Quaternion.Slerp(myPosition.rotation, Quaternion.LookRotation(target.position - myPosition.position),rotationSpeed * Time.deltaTime);
+	}
+}
