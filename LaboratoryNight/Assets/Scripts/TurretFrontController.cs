4ba11559d9dd38836f8613625394d3ff8db88dc2
Mariusz Hausenplas <xelitas@gmail.com>
2015-04-08 15:41:04 +0200

    ponowne dodanie turreta do TestScene

---
+++ LaboratoryNight/Assets/Scripts/TurretFrontController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretFrontController : MonoBehaviour {
+
+    public GameObject rocket;
+    public GameObject efx;
+
+	void Start () 
+    {
+        StartCoroutine("ShootRocket");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    private IEnumerator ShootRocket()
+    {
+        while (true)
+        {
+            GameObject obj = Instantiate(rocket, transform.position, Quaternion.identity) as GameObject;
+            GameObject efxObj = Instantiate(efx, transform.position, Quaternion.identity) as GameObject;
+            Destroy(efxObj, 0.3f);
+
+            obj.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.Impulse);
+            StartCoroutine("AccelerateRocket", obj);
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private IEnumerator AccelerateRocket(GameObject obj)
+    {
+        yield return new WaitForSeconds(0.4f);
+        obj.GetComponent<Rigidbody>().AddForce(transform.forward * 40f, ForceMode.Impulse);
+    }
+
+}
---
+++ LaboratoryNight/Assets/Scripts/TurretRotationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretRotationController : MonoBehaviour {
+
+
+    private GameObject player;
+	void Start () 
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () 
+    {
+        transform.LookAt(player.transform.position);
+	}
+}
