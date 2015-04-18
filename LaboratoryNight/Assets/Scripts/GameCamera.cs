0fea7c4ef5fa2a26dcf558bbfaa6822a2e1de842
Mariusz Hausenplas <xelitas@gmail.com>
2015-04-18 22:17:17 +0200

    nowa kamera - proba

--- LaboratoryNight/Assets/Scripts/GameCamera.cs
+++ LaboratoryNight/Assets/Scripts/GameCamera.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GameCamera : MonoBehaviour {
 
 	private Vector3 cameraTarget;
-    private const float CAMERA_Z_OFFSET = 0.2f;
+    private const float CAMERA_Z_OFFSET = 10f;
 	private Transform target;
     private float camY;
 
