1c1b912b879fbeafd8d447f7988ded7092433b24
unknown <akalbarczyk@hotmail.com>
2015-05-19 13:08:20 +0200

    door + scripts

---
+++ LaboratoryNight/Assets/UnlockFirstDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UnlockFirstDoor : MonoBehaviour {
+    public Text text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.tag == "Player")
+        {            
+            text.text = "press space to hack the computer";
+            if (Input.GetKey(KeyCode.Space))
+            {
+                text.text = "asdddddddddddddddddddd";
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.gameObject.tag == "Player")
+        {
+            text.text = "";
+        }
+    }
+}
