537fba8c53ec84bd54afecbcbd3547addf24a8e8
unknown <akalbarczyk@hotmail.com>
2015-05-24 19:27:23 +0200

    design levela, poprawione textury itp

---
+++ LaboratoryNight/Assets/ThirdClosedDoor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ThirdClosedDoor : MonoBehaviour {
+
+    public Text text;
+    private bool isDoorOpening = false;
+    private bool isDoorClosing = false;
+
+    public static bool isAllowed;
+
+    private Vector3 basePosition;
+    private Vector3 upPosition;
+
+    void Start()
+    {
+
+        basePosition = transform.position;
+        upPosition = new Vector3(basePosition.x, basePosition.y + 10, basePosition.z);
+    }
+
+
+    void Update()
+    {
+
+        if (isDoorOpening)
+        {
+            transform.position = Vector3.Lerp(transform.position, upPosition, Time.deltaTime * 3f);
+        }
+
+        if (isDoorClosing)
+        {
+            transform.position = Vector3.Lerp(transform.position, basePosition, 0.02f);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider target)
+    {
+        if ((target.gameObject.tag == "Player" && isAllowed) || target.gameObject.tag == "Enemy")
+        {
+            if (!isDoorOpening)
+            {
+                isDoorOpening = true;
+                isDoorClosing = false;
+            }
+        }
+        if (target.gameObject.tag == "Player" && !isAllowed)
+        {
+            text.text = "you must unlock the third first";
+            Debug.Log("test");
+        }
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.gameObject.tag == "Player" || target.gameObject.tag == "Enemy")
+        {
+            if (!isDoorClosing)
+            {
+                isDoorClosing = true;
+                isDoorOpening = false;
+            }
+
+        }
+        if (target.gameObject.tag == "Player")
+            text.text = "";
+    }
+}
---
+++ LaboratoryNight/Assets/UnlockThirdDoor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UnlockThirdDoor : MonoBehaviour {
+
+    public Text text;
+    bool canPress;
+
+    public GameObject bootEffect;
+
+    // Use this for initialization
+    void Start()
+    {
+        canPress = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (canPress && (Input.GetKeyDown("space")))
+        {
+            ThirdClosedDoor.isAllowed = true;
+            canPress = false;
+            GameObject boot = Instantiate(bootEffect, transform.position + transform.up * 4, transform.rotation) as GameObject;
+            Destroy(boot, 0.3f);
+            text.text = "";
+        }
+    }
+
+    void OnTriggerEnter(Collider target)
+    {
+        if ((target.gameObject.tag == "Player") && !ThirdClosedDoor.isAllowed)
+        {
+            text.text = "press space to hack the computer";
+            canPress = true;
+        }
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.gameObject.tag == "Player")
+        {
+            text.text = "";
+            canPress = false;
+        }
+    }
+}
