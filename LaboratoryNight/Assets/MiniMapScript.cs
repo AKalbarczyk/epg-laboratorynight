6a12d15f754e9b548fde5e2d53ee0db8fbba491a
unknown <akalbarczyk@hotmail.com>
2015-05-26 23:19:44 +0200

    Poprawiona minimapa + bugfixy

---
+++ LaboratoryNight/Assets/MiniMapScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapScript : MonoBehaviour {
+    public Shader unlitShader;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Camera>().SetReplacementShader(unlitShader, "");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
