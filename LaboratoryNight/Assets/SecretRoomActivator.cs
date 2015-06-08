5f13eb502593e55139320c4588290b1f2eb3fef8
unknown <akalbarczyk@hotmail.com>
2015-06-08 20:58:29 +0200

    Poprawiony sRoom, dodane przejscie z drugiego levela do sRoom + skrypty

---
+++ LaboratoryNight/Assets/SecretRoomActivator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretRoomActivator : MonoBehaviour {
+    public GameObject efx;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            GameObject obj = Instantiate(efx, transform.position + transform.right * 8 + transform.up *3, Quaternion.identity) as GameObject;
+            
+        }
+    }
+}
---
+++ LaboratoryNight/Assets/TakeToSecretRoom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeToSecretRoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+         //   GameObject obj = Instantiate(efx, transform.position, Quaternion.identity) as GameObject;
+            Invoke("LoadNextLevel", 0.6f);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 2);
+    }
+}
