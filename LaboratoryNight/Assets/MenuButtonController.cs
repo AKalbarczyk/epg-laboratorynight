a54823b75146ca27a32e684fcbd87250eaa574ad
Mariusz Hausenplas <xelitas@gmail.com>
2015-06-16 21:32:56 +0200

    dodane menu

---
+++ LaboratoryNight/Assets/MenuButtonController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuButtonController : MonoBehaviour {
+
+    private Text text;
+
+	void Start () 
+    {
+        text = GetComponent<Text>();
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    public void OnMouseEnter()
+    {
+        text.color = Color.magenta;
+    }
+
+    public void OnMouseExit()
+    {
+        text.color = Color.white;
+    }
+
+    void OnMouseDown()
+    {
+        
+    }
+
+
+
+    public void TextAction()
+    {
+        if (this.gameObject.name == "Start")
+        {
+            Application.LoadLevel(0);
+        }
+
+        else if (this.gameObject.name == "Exit")
+        {
+            Application.Quit();
+        }
+    }
+}
