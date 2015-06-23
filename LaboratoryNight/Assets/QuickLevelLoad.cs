94cfac697ee9d2491baa4992c9f0c227bac87b44
unknown <akalbarczyk@hotmail.com>
2015-06-23 19:18:45 +0200

    BugFixy, loadlevele, loadLevel screens, quick level access

---
+++ LaboratoryNight/Assets/QuickLevelLoad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickLevelLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.F1))
+        {
+            Application.LoadLevel(1);
+        }
+        else if (Input.GetKey(KeyCode.F2))
+        {
+            Application.LoadLevel(2);
+        }
+        else if (Input.GetKey(KeyCode.F3))
+        {
+            Application.LoadLevel(3);
+        }
+	}
+}
--- LaboratoryNight/Assets/Scripts/KeysInfo.cs
+++ LaboratoryNight/Assets/Scripts/KeysInfo.cs
@@ -19,6 +19,7 @@ public class KeysInfo : MonoBehaviour {
         {
             img.enabled = false;
             text.enabled = false;
+            
             Destroy(this.gameObject);
         }
 	
